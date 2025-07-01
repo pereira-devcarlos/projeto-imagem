ca6eb2cd8ed4ee15b7c5329e9bf6a69068cb0127
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-07-01 14:39:26 -0300

    feat: implementar menu interativo para carregamento e manipulação de imagens

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,39 @@
 using namespace std;
 
 int main(){
-    
-    
+    typedef int Imagem[1024][1024];
+
+    string nomeArquivo;
+
+    int opcao=1;
+    while (opcao != 0){
+        cout << "====== Menu de Opcoes ======" << endl;
+        cout << "[1] - Carregar Imagem" << endl;
+        cout << "[2] - Menu de Alteracoes da Imagem" << endl;
+        cout << "[0] - Encerrar Programa" << endl;
+        cout << "============================" << endl;
+        cout << "Escolha uma opcao: ";
+        cin >> opcao;
+
+        switch (opcao) {
+            case 1:
+                cout << "O formato do nome do arquivo deve ser: 'imagem.pgm'" << endl;
+                cout << "Digite o nome do arquivo: ";
+                cin >> nomeArquivo;
+                nomeArquivo += ".pgm";
+                cout << "Carregando imagem: " << nomeArquivo << endl;
+                break;
+            case 2:
+                // Menu de Alterações da Imagem
+                break;
+            case 0:
+                cout << "Encerrando o programa..." << endl;
+                break;
+            default:
+                cout << "Opcao invalida. Tente novamente." << endl;
+                break;
+        }
+    }
+
     return 0;
 }
